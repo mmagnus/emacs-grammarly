c68033a33509dabd757435672832d34232b71361
Lars Tveito <larstvei@ifi.uio.no>
2017-09-05 00:48:07 +0200

    Use a temporary file per buffer unless user has specified a default
    Addresses #1

--- emacs-grammarly.el
+++ emacs-grammarly.el
@@ -3,14 +3,22 @@
 ;;   URL: http://example.com/jrhacker/superfrobnicate
 ;;; Code:
 
-(defvar grammarly-tempfile "/home/magnus/Desktop/Grammarly.txt")
+(defvar grammarly-default-file nil
+  "If non-nil, `grammarly-save-region-and-run' always writes to this file.")
+
+(defvar-local grammarly-buffer-file nil
+  "The temporary file associated with the current buffer.")
+
 (defvar grammarly-cmd "open -a Grammarly")
 
 (defun grammarly-save-region-and-run ()
   "Save region to a tempfile and run Grammarly on it."
   (interactive)
-  (write-region (region-beginning) (region-end) grammarly-tempfile)
-  (call-process-shell-command (concat grammarly-cmd " " grammarly-tempfile)))
+  (let ((file (or grammarly-default-file grammarly-buffer-file
+                  (setq grammarly-buffer-file
+                        (make-temp-file (buffer-name) nil ".txt")))))
+    (write-region (region-beginning) (region-end) file)
+    (call-process-shell-command (concat grammarly-cmd " " file))))
 
 (provide 'emacs-grammarly)
 ;;; emacs-grammarly.el ends here
