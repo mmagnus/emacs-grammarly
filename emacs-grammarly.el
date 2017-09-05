3eac3725d81fa0ad323292a2ef06310447e6d250
Lars Tveito <larstvei@ifi.uio.no>
2017-09-05 02:15:18 +0200

    Use a single temporary file for Grammarly
    Sadly, reloading only works for updating the same file, so having one for each
    buffer does not work with the current solution.
    
    There is still a problem with `open -a Grammarly` not doing anything if
    Grammarly is already open, so the solution assumes that Grammarly is closed or
    that the user is visiting the generated file in Grammarly.

--- emacs-grammarly.el
+++ emacs-grammarly.el
@@ -3,26 +3,24 @@
 ;;   URL: http://example.com/jrhacker/superfrobnicate
 ;;; Code:
 
-(defvar grammarly-default-file nil
-  "If non-nil, `grammarly-save-region-and-run' always writes to this file.")
-
-(defvar-local grammarly-buffer-file nil
-  "The temporary file associated with the current buffer.")
+(defvar grammarly-file nil
+  "The temporary file for storing things sent to Grammarly.")
 
 (defvar grammarly-cmd "open -a Grammarly")
+
 (defvar grammarly-reload-cmd "osascript <<END
 tell application \"Grammarly\" to activate
 tell application \"System Events\"
-	keystroke \"r\" using command down
+        keystroke \"r\" using command down
 end tell
 END")
 
 (defun grammarly-save-region-and-run ()
   "Save region to a tempfile and run Grammarly on it."
   (interactive)
-  (let ((file (or grammarly-default-file grammarly-buffer-file
-                  (setq grammarly-buffer-file
-                        (make-temp-file (buffer-name) nil ".txt")))))
+  (let ((file (or grammarly-file
+                  (setq grammarly-file
+                        (make-temp-file "grammarly" nil ".txt")))))
     (write-region (region-beginning) (region-end) file)
     (call-process-shell-command
      (concat grammarly-cmd " " file ";" grammarly-reload-cmd))))
