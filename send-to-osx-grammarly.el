ead7d5a6176a0331a3b0456c4704761cf8c93495
JenChieh <jcs090218@gmail.com>
2021-02-23 20:13:34 +0800

    Update info

--- send-to-osx-grammarly.el
+++ send-to-osx-grammarly.el
@@ -1,7 +1,38 @@
-;;; emacs-grammarly.el --- a simple plugin to sent text to Grammarly
+;;; send-to-osx-grammarly.el --- A simple plugin to sent text to Grammarly  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-02-23 20:08:40
+
+;; Author: Marcin Magnus <m.magnus@zoho.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: A simple plugin to sent text to Grammarly.
+;; Keyword: osx grammarly
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/emacs-grammarly/send-to-osx-grammarly
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;;   URL: https://github.com/mmagnus/emacs-grammarly
+;;
+;; A simple plugin to sent text to Grammarly.
+;;
+
 ;;; Code:
+
 (defun grammarly-push ()
   "Save region to a tempfile and run Grammarly on it."
   (interactive)
@@ -17,8 +48,5 @@
   (yank)
   )
 
-(global-set-key (kbd "C-c C-g h") 'grammarly-push)
-(global-set-key (kbd "C-c C-g l") 'grammarly-pull)
-
-(provide 'emacs-grammarly)
-;;; emacs-grammarly.el ends here
+(provide 'send-to-osx-grammarly)
+;;; send-to-osx-grammarly.el ends here
